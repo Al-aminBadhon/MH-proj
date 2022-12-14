b723d2848a208b557dfcf101bea677ad3ac1ed53
Badhon-PC <Badhon@DESKTOP-OK3LNJL>
2022-12-14 23:25:26 +0600

    changes

--- MH proj/Controllers/HomeController.cs
+++ MH proj/Controllers/HomeController.cs
@@ -438,6 +438,9 @@ namespace MH_proj.Controllers
             Console.WriteLine("Text added for sync new one asdadasdadasdasdasd111111222 ");
             Console.WriteLine("Text added for sync new one asdadasdadasdasdasd111111222 ");
             Console.WriteLine("Text added for sync new one asdadasdadasdasdasd111111222 ");
+            Console.WriteLine("Text added for sync new one asdadasdadasdasdasd111111222 ");
+            Console.WriteLine("Text added for sync new one asdadasdadasdasdasd111111222 ");
+            Console.WriteLine("Text added for sync new one asdadasdadasdasdasd111111222 ");
             
 
 
